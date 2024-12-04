cee84b6f6d4695086c80e2e87ed83e8fe8c7a119
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-04 02:33:47 -0600

    modulization

--- app.R
+++ app.R
@@ -8,6 +8,7 @@ source("global_setup.R")
 source("data_loading.R")
 source("drive_functions.R")
 source("barcode_functions.R")
+source("notifications.R")
 
 # Google Auth setup
 setup_google_auth("goldenbeanllc.bhs@gmail.com")
