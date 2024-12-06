3558217c402fc0fa7c1a55a266cc4cb7aef5b268
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-06 10:22:13 -0600

    Update utils.R

--- utils.R
+++ utils.R
@@ -8,9 +8,15 @@ setup_google_auth <- function(email) {
 # Map column names
 map_column_names <- function(data, column_mapping) {
   existing_columns <- names(data)
+  
   mapped_columns <- sapply(existing_columns, function(col) {
-    column_mapping[[col]] %||% col  # 如果没有映射，保留原始列名
+    if (!is.null(column_mapping[[col]])) {
+      return(column_mapping[[col]])  # 如果有映射，使用映射的列名
+    } else {
+      return(col)  # 否则，保留原始列名
+    }
   })
+  
   setNames(data, mapped_columns)  # 更新列名
 }
 
