0d897523947a18a9871d81a7c25406ae34f2b2cb
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-12 00:04:19 -0600

    Update server.R

--- server.R
+++ server.R
@@ -576,7 +576,7 @@ server <- function(input, output, session) {
     input$new_name
     input$new_maker
   }, {
-    req(input$new_major_type, input$new_minor_type, input$new_name, input$new_maker)
+    # req(input$new_major_type, input$new_minor_type, input$new_name, input$new_maker)
     
     showNotification(paste0("[Debug]", 
                            input$new_major_type, 
