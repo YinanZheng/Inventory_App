94d34a12bb389424fb160b93c25c490b3c52766a
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-14 04:07:30 -0600

    new ui

--- modules/typeModuleUI.R
+++ modules/typeModuleUI.R
@@ -6,14 +6,14 @@ typeModuleUI <- function(id) {
       column(1, 
              div(style = "display: flex; justify-content: center; align-items: center; height: 100%;", 
                  actionButton(ns("add_major_type_btn"), label = NULL, icon = icon("plus"),
-                              style = "font-size: 14px; width: 100%; max-width: 40px; height: 34px; padding: 0px; margin-top: 25px;")
+                              style = "font-size: 14px; width: 100%; max-width: 70px; height: 32px; padding: 0px; margin-top: 27px;")
              )
       ),
       column(5, uiOutput(ns("minor_type_ui"))),  # 小类下拉框
       column(1, 
              div(style = "display: flex; justify-content: center; align-items: center; height: 100%;", 
                  actionButton(ns("add_minor_type_btn"), label = NULL, icon = icon("plus"),
-                              style = "font-size: 14px; width: 100%; max-width: 40px; height: 34px; padding: 0px; margin-top: 25px;")
+                              style = "font-size: 14px; width: 100%; max-width: 70px; height: 32px; padding: 0px; margin-top: 27px;")
              )
       )
     )
