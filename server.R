8f6f22b1df71ee1303893fa1918b4f67cac3b396
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-14 04:53:26 -0600

    Update server.R

--- server.R
+++ server.R
@@ -554,7 +554,7 @@ server <- function(input, output, session) {
             style = "text-align: center;",
             img(
               src = img_path, 
-              height = "280px", 
+              height = "300px", 
               style = "border: 2px solid #ddd; border-radius: 8px; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);"
             ),
           )
