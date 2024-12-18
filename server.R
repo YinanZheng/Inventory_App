0bbfd04b35d7d4fd982e0c90bd598f9630203732
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-18 13:16:17 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1276,12 +1276,29 @@ server <- function(input, output, session) {
   ################################################################
   # 
   # 初始化筛选选项
-  updateSelectInput(session, "maker", choices = unique(unique_items_data()$Maker), selected = unique(unique_items_data()$Maker))
-  updateSelectInput(session, "unique_status", choices = unique(unique_items_data()$Status), selected = unique(unique_items_data()$Status))
-  updateSelectInput(session, "unique_defect", choices = unique(unique_items_data()$Defect), selected = unique(unique_items_data()$Defect))
-  updateSelectInput(session, "major_type", choices = unique(unique_items_data()$MajorType), selected = unique(unique_items_data()$MajorType))
-  updateSelectInput(session, "minor_type", choices = unique(unique_items_data()$MinorType), selected = unique(unique_items_data()$MinorType))
-
+  if (!is.null(unique_items_data())) {
+    updateSelectInput(session, "maker", 
+                      choices = unique(unique_items_data()$Maker), 
+                      selected = unique(unique_items_data()$Maker)
+    )
+    updateSelectInput(session, "unique_status", 
+                      choices = unique(unique_items_data()$Status), 
+                      selected = unique(unique_items_data()$Status)
+    )
+    updateSelectInput(session, "unique_defect", 
+                      choices = unique(unique_items_data()$Defect), 
+                      selected = unique(unique_items_data()$Defect)
+    )
+    updateSelectInput(session, "major_type", 
+                      choices = unique(unique_items_data()$MajorType), 
+                      selected = unique(unique_items_data()$MajorType)
+    )
+    updateSelectInput(session, "minor_type", 
+                      choices = unique(unique_items_data()$MinorType), 
+                      selected = unique(unique_items_data()$MinorType)
+    )
+  }
+  
   # # 筛选逻辑
   # filtered_unique_items_data <- reactive({
   #   data <- unique_items_data()
