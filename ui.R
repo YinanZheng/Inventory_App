c696b844047288c1f54317dfea54dd8bc4c96ee6
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-19 04:31:50 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -150,7 +150,7 @@ ui <- navbarPage(
                  conditionalPanel(
                    condition = "input.inventory_switch == true",  # JavaScript 条件
                    div(
-                     style = "height: 50px; display: flex; align-items: center;",
+                     style = "height: 50px; display: flex;",
                      dateInput(
                        inputId = "inventory_date",
                        label = "采购日期:",
