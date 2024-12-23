d0c586d93501c799fc2b927e9d08d3ac39a85b75
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-23 11:02:16 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1913,7 +1913,7 @@ server <- function(input, output, session) {
       for (i in seq_len(nrow(final_data))) {
         image_path <- final_data[i, col_to_insert]
         image_width_max <- 1
-        if (!is.na(image_path) && file.exists(image_path)) {
+        # if (!is.na(image_path) && file.exists(image_path)) {
           
           # 获取图片的实际宽高比
           dims <- get_image_dimensions(image_path)
@@ -1943,9 +1943,9 @@ server <- function(input, output, session) {
           # 调整行高和列宽
           setRowHeights(wb, "物品明细表", rows = row_to_insert, heights = image_height * 78)
 
-        } else {
-          showNotification(paste("跳过不存在的图片:", image_path), type = "warning", duration = 5)
-        }
+        # } else {
+        #   showNotification(paste("跳过不存在的图片:", image_path), type = "warning", duration = 5)
+        # }
       }
 
       # 最终设置列宽，保证所有图片适配最大宽度
