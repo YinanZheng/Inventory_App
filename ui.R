c197db05b8a6a1deb12d505dc48c2d3dc075c18d
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-27 10:54:42 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -510,7 +510,7 @@ ui <- navbarPage(
                    
                    fluidRow(
                      column(
-                       width = 8, # 左侧按钮宽度
+                       width = 7, # 左侧按钮宽度
                        actionButton(
                          "confirm_order_btn",
                          "确认售出",
@@ -520,7 +520,7 @@ ui <- navbarPage(
                        )
                      ),
                      column(
-                       width = 4, # 右侧选择框宽度
+                       width = 5, # 右侧选择框宽度
                        tags$div(
                          style = "
         border: 1px solid #007BFF; 
