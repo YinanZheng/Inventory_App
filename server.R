a489179cc6cec74cb66ca4257152e27647bf8065
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-27 01:47:13 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1021,42 +1021,11 @@ server <- function(input, output, session) {
   
   
   ######
-  
-  # itemFilterServer("sold",
-  #                  unique_items = unique_items_data,
-  #                  makers = makers_df,
-  #                  selected_row_reactive = unique_items_table_sold_selected_row)
-  # 
-  # 更新供应商名称
-  # observeEvent(makers_df(), {
-  #   update_maker_choices(session, ns("maker"), makers_df())
-  # })
 
+  # 更新makers控件  
   observeEvent(makers_df(), {
     update_maker_choices(session, "sold-maker", makers_df())
   })
-  
-  
-  # # 监听供应商选择变化并动态更新商品名称
-  # observe({
-  #   req(unique_items_data())  # 确保数据存在
-  #   
-  #   # 获取用户选择的供应商
-  #   selected_makers <- input$sold_maker
-  #   
-  #   # 筛选商品名称
-  #   if (!is.null(selected_makers) && length(selected_makers) > 0) {
-  #     filtered_data <- unique_items_data() %>% filter(Maker %in% selected_makers)
-  #   } else {
-  #     filtered_data <- unique_items_data()
-  #   }
-  #   
-  #   # 提取对应的商品名称，并在前面加一个空选项
-  #   item_names <- c("", filtered_data %>% pull(ItemName) %>% unique())
-  #   
-  #   # 更新商品名称选项，默认选中空选项
-  #   updateSelectizeInput(session, "sold_name", choices = item_names, selected = "")
-  # })
   
   # 监听供应商选择变化并动态更新商品名称
   observe({
@@ -1080,20 +1049,6 @@ server <- function(input, output, session) {
     updateSelectizeInput(session, "sold-name", choices = item_names, selected = "")
   })
   
-  
-  # 
-  # # 监听选中行并更新 maker, item name, SKU
-  # observeEvent(unique_items_table_sold_selected_row(), {
-  #   if (!is.null(unique_items_table_sold_selected_row()) && length(unique_items_table_sold_selected_row()) > 0) {
-  #     selected_data <- filtered_unique_items_data_sold()[unique_items_table_sold_selected_row(), ]
-  #     updateSelectInput(session, "sold_maker", selected = selected_data$Maker)
-  #     shinyjs::delay(300, {  # 延迟 300 毫秒
-  #       updateTextInput(session, "sold_name", value = selected_data$ItemName)
-  #     })
-  #     updateTextInput(session, "sold_sku", value = selected_data$SKU)
-  #   }
-  # })
-  
   # 监听选中行并更新 maker, item name, SKU
   observeEvent(unique_items_table_sold_selected_row(), {
     if (!is.null(unique_items_table_sold_selected_row()) && length(unique_items_table_sold_selected_row()) > 0) {
@@ -1105,20 +1060,7 @@ server <- function(input, output, session) {
       updateTextInput(session, "sold-sku", value = selected_data$SKU)
     }
   })
-  
-  # # 清空输入
-  # observeEvent(input$sold_reset_btn, {
-  #   tryCatch({
-  #     update_maker_choices(session, "sold_maker", makers_df())
-  #     updateTextInput(session, "sold_name", value = "")
-  #     updateTextInput(session, "sold_sku", value = "")
-  #     
-  #     showNotification("筛选条件已重置！", type = "message")
-  #   }, error = function(e) {
-  #     showNotification("重置输入时发生错误，请重试！", type = "error")
-  #   })
-  # })
-  
+
   # 清空输入
   observeEvent(input$`sold-reset_btn`, {
     tryCatch({
@@ -1131,6 +1073,7 @@ server <- function(input, output, session) {
       showNotification("重置输入时发生错误，请重试！", type = "error")
     })
   })
+  
   
   ######
   
