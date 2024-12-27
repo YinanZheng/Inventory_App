0a30cd80b032ee6cb0bfbd0ae395522f336c5b69
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-27 02:48:26 -0600

    update

--- server.R
+++ server.R
@@ -1028,7 +1028,7 @@ server <- function(input, output, session) {
     makers_df = makers_df,
     unique_items_data = unique_items_data,
     filtered_unique_items_data = filtered_unique_items_data_sold,
-    unique_items_table_selected_row = unique_items_table_sold_selected_row,
+    unique_items_table_selected_row = unique_items_table_sold_selected_row
   )
   
   # # 更新makers控件  
