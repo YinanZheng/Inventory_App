91698c2d21823df6f6d4717c5f91a1419d7f355e
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-27 09:23:05 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -323,7 +323,6 @@ ui <- navbarPage(
         class = "main-panel",
         
         div(
-          class = "sticky-info",  
           column(12, uiOutput("inbound_item_info"), style = "margin-bottom: 10px;") # 动态渲染物品信息
         ), 
         
