c01b73234761eab60ac65fef94723d299f92928e
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-29 17:46:31 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1168,10 +1168,10 @@ server <- function(input, output, session) {
     }
   })
   
-  # # 动态填充供应商选择器
-  # observe({
-  #   update_maker_choices(session, "preorder_supplier", maker_data())
-  # })
+  # 动态填充供应商选择器
+  observe({
+    update_maker_choices(session, "preorder_supplier", maker_list())
+  })
   
   # 控制预订单供应商选择器的显示
   observeEvent(input$is_preorder, {
