a3bba754a82fecfd0bc72fbd35ab56a618b8106d
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-29 14:08:14 -0600

    Update utils.R

--- utils.R
+++ utils.R
@@ -813,6 +813,8 @@ register_order <- function(order_id, customer_name, customer_netname, platform, 
     # 合并订单关联物品和发货箱的图片路径
     combined_image_paths <- unique(c(order_image_paths, box_image_paths))
     
+    showNotification(paste0("正在拼贴 ", length(combined_image_paths), " 张物品图..."), type = "messsage")
+    
     if (nrow(existing_order) > 0) {
       # 如果订单已存在
       existing_orders_path <- existing_order$OrderImagePath[1]
