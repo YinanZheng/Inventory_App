1b6a155f4f71856699b2b9c6fd0207eb180b1397
Yinan Zheng <y-zheng@northwestern.edu>
2024-12-31 12:15:09 -0600

    Update itemFilterUI.R

--- modules/itemFilterUI.R
+++ modules/itemFilterUI.R
@@ -5,7 +5,19 @@ itemFilterUI <- function(id, border_color = "#007BFF", text_color = "#007BFF", u
     class = "card",
     style = sprintf("margin-bottom: 5px; padding: 5px; border: 1px solid %s; border-radius: 8px; box-shadow: 0px 4px 6px rgba(0,0,0,0.1);", border_color),
     
-    tags$h4("物品筛选", style = sprintf("color: %s; font-weight: bold; margin-bottom: 15px;", text_color)),
+    # 物品筛选标题和清空按钮
+    fluidRow(
+      column(9, 
+             tags$h4("物品筛选", style = sprintf("color: %s; font-weight: bold; margin-bottom: 15px;", text_color))
+      ),
+      column(3,
+             div(
+               style = "text-align: right;",
+               actionButton(ns("reset_btn"), "清空", icon = icon("snowplow"), class = "btn-danger", 
+                            style = "font-size: 14px; width: auto; height: 35px; padding: 5px 10px; margin-bottom: 10px;")
+             )
+      )
+    ),
     
     fluidRow(
       column(6, 
@@ -38,13 +50,6 @@ itemFilterUI <- function(id, border_color = "#007BFF", text_color = "#007BFF", u
       )
     } else {
       NULL  # 如果不使用采购日期筛选，隐藏该部分
-    },
-    
-    # 清空按钮部分
-    div(
-      style = "text-align: right; margin-top: 15px;",
-      actionButton(ns("reset_btn"), "清空", icon = icon("snowplow"), class = "btn-danger", 
-                   style = "font-size: 14px; width: auto; height: 35px; padding: 5px 20px;")
-    )
+    }
   )
 }
