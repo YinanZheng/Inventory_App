8bb16abe1ca7aca6e7d27f7d1c1f37b898ace69e
Yinan Zheng <y-zheng@northwestern.edu>
2025-01-06 10:11:22 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1602,7 +1602,7 @@ server <- function(input, output, session) {
                              selection = "single",
                              image_column = "ItemImagePath",
                              options = list(
-                               scrollY = "270px",  # 根据内容动态调整滚动高度
+                               scrollY = "250px",  # 根据内容动态调整滚动高度
                                scrollX = TRUE,  # 支持水平滚动
                                fixedHeader = TRUE,  # 启用表头固定
                                paging = TRUE,  # 启用分页
