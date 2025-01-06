852f2062a8db55950585dd749a5d6299315249e0
Yinan Zheng <y-zheng@northwestern.edu>
2025-01-06 10:28:15 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -581,7 +581,7 @@ ui <- navbarPage(
                        class = "card",
                        style = "padding: 10px; margin-bottom: 5px; border: 1px solid #28A745; border-radius: 10px; box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);",
                        div(
-                         style = "display: flex; align-items: center; justify-content: space-between; margin-bottom: 0px; height: 21px",
+                         style = "display: flex; align-items: center; justify-content: space-between; margin-bottom: 0px; height: 30px",
                          
                          # 发货箱标题和动态显示数量
                          tags$h4(
