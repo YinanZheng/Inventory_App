c43b9f29a15e75b69dbe48baeba6eacc747f38cb
Yinan Zheng <y-zheng@northwestern.edu>
2025-01-13 07:58:59 -0600

    Update server.R

--- server.R
+++ server.R
@@ -3659,8 +3659,9 @@ server <- function(input, output, session) {
           color = ~ifelse(AllPurchaseCheck, "#039e2a", "#D3D3D3"), # 深绿色和浅灰色
           weight = "bold" # 加粗字体
         ),
-        marker = NULL, # 明确移除 marker 属性
-        showlegend = FALSE # 不显示图例
+        marker = list(
+          opacity = 0 # 设置透明度为 0，完全隐藏 marker
+        ),        showlegend = FALSE # 不显示图例
       ) %>%
       # 添加布局和其他设置
       layout(
