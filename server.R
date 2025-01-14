302fce43567d2e0aa5a25bd4df1fd21ba5fbf3f6
Yinan Zheng <y-zheng@northwestern.edu>
2025-01-14 08:48:15 -0600

    Update server.R

--- server.R
+++ server.R
@@ -2292,11 +2292,6 @@ server <- function(input, output, session) {
             style = "margin-left: auto; font-size: 14px; padding: 5px 10px;"
           )
         }
- 
-        if(!is.na(label_pdf_file_path()) && label_pdf_file_path() != "") {
-          downloadButton("download_pdf_manage", label = "下载运单", class = "btn btn-primary", 
-                         style = "height: 34px; margin-left: 10px; font-size: 14px; padding: 5px 10px;")
-        }
       )
     })
     
