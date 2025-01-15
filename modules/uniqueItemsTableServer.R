a4bc19d4fcd1c9c279204bfbcb3c9cd6ba33e911
Yinan Zheng <y-zheng@northwestern.edu>
2025-01-15 02:29:15 -0600

    Update uniqueItemsTableServer.R

--- modules/uniqueItemsTableServer.R
+++ modules/uniqueItemsTableServer.R
@@ -13,6 +13,31 @@ uniqueItemsTableServer <- function(input, output, session, column_mapping, selec
     table
   }, server = TRUE)
   
+  # 监听用户点击图片列
+  observeEvent(input$unique_items_table_cell_clicked, {
+    info <- input$unique_items_table_cell_clicked
+    
+    # 检查是否点击了图片列（第三列）
+    if (!is.null(info) && !is.null(info$col) && !is.null(info$row)) {
+      if (info$col == 2) {  # 第三列在 R 中的索引是 2
+        # 获取点击的图片路径
+        img_path <- data()[info$row, "ItemImagePath"]
+        req(img_path)  # 确保图片路径存在
+        
+        img_host_path <- paste0(host_url, "/images/", basename(img_path))
+        
+        # 弹出窗口显示大图
+        showModal(modalDialog(
+          title = "订单图片预览",
+          img(src = img_host_path, height = "500px", style = "display: block; margin: 0 auto;"),
+          size = "l",
+          easyClose = TRUE,
+          footer = modalButton("关闭")
+        ))
+      }
+    }
+  })
+  
   # 返回选中行的索引
   reactive({
     input$unique_items_table_rows_selected
