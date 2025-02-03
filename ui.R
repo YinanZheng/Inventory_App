6cf972487d77f2fb11557a264aa54aac99b1604b
Yinan Zheng <y-zheng@northwestern.edu>
2025-02-03 07:22:59 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -1354,7 +1354,7 @@ ui <- navbarPage(
                 div(
                   class = "card",
                   style = "margin-bottom: 5px; padding: 5px; border: 1px solid #28a745; border-radius: 8px; box-shadow: 0px 4px 6px rgba(0,0,0,0.1);",
-                  tags$h4("库存状态图表", style = "color: #28a745; font-weight: bold; padding-left: 10px;"),
+                  tags$h4("库存状态", style = "color: #28a745; font-weight: bold; padding-left: 10px;"),
                   plotlyOutput("inventory_status_chart", height = "320px") # 使用 plotlyOutput
                 )
               ),
@@ -1364,7 +1364,7 @@ ui <- navbarPage(
                 div(
                   class = "card",
                   style = "margin-bottom: 5px; padding: 5px; border: 1px solid #dc3545; border-radius: 8px; box-shadow: 0px 4px 6px rgba(0,0,0,0.1);",
-                  tags$h4("瑕疵情况图表", style = "color: #dc3545; font-weight: bold; padding-left: 10px"),
+                  tags$h4("瑕疵情况", style = "color: #dc3545; font-weight: bold; padding-left: 10px"),
                   plotlyOutput("defect_status_chart", height = "320px") # 使用 plotlyOutput
                 )
               )
