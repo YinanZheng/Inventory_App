c13d8aeb97d70331860938cd876dd3d130310741
Yinan Zheng <y-zheng@northwestern.edu>
2025-02-18 05:51:15 -0600

    Update server.R

--- server.R
+++ server.R
@@ -1542,6 +1542,9 @@ server <- function(input, output, session) {
       # **删除 `OrderNotes` 里匹配的 `item_name`**
       updated_notes <- trimws(sub("，；", "；", sub("】，", "】", sub("，，", "，", sub(preorder_info$item_name, "", current_notes, fixed = TRUE)))))
       
+      # **如果 `updated_notes` 仅剩 `"【预定物品】；"`，改为 `"【预定物品登记完毕】"`**
+      updated_notes <- sub("【预定物品】；", "【预定物品登记完毕】", updated_notes, fixed = TRUE)
+      
       # **更新 `OrderNotes`**
       dbExecute(con, 
                 "UPDATE orders SET OrderNotes = ? WHERE OrderID = ?", 
