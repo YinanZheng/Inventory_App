ca91c22acfad2631430d851626bc99cd980b733c
Yinan Zheng <y-zheng@northwestern.edu>
2025-02-23 16:57:28 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -518,7 +518,7 @@ ui <- navbarPage(
       class = "layout-container",  # Flexbox 容器
       div(
         class = "sticky-sidebar",  # sticky 侧边栏
-        itemFilterUI(id = "inbound_filter", border_color = "#28A745", text_color = "#28A745", status_choices = c("", "采购", "国内入库")),
+        itemFilterUI(id = "inbound_filter", border_color = "#28A745", text_color = "#28A745", status_choices = c("库存状态" = "", "采购", "国内入库")),
         
         tags$hr(style = "margin: 5px 0; border: none;"),
         
@@ -648,7 +648,7 @@ ui <- navbarPage(
       class = "layout-container",  # Flexbox 容器
       div(
         class = "sticky-sidebar",  # sticky 侧边栏
-        itemFilterUI(id = "outbound_filter", border_color = "#28A745", text_color = "#28A745", status_choices = c("", "国内入库", "国内出库")),
+        itemFilterUI(id = "outbound_filter", border_color = "#28A745", text_color = "#28A745", status_choices = c("库存状态" = "", "国内入库", "国内出库")),
         
         tags$hr(style = "margin: 5px 0; border: none;"),
         
@@ -768,7 +768,7 @@ ui <- navbarPage(
           tabPanel(
             title = "物品筛选",
             itemFilterUI(id = "sold_filter", border_color = "#28A745", text_color = "#28A745",
-                         status_choices = c("", "国内入库", "国内出库", "美国入库", "美国调货", "国内售出"))
+                         status_choices = c("库存状态" = "", "国内入库", "国内出库", "美国入库", "美国调货", "国内售出"))
           )
         ),
         
