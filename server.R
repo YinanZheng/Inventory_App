0ef098f35b2eda69f7d1301815976c8e013eda17
Yinan Zheng <y-zheng@northwestern.edu>
2025-03-12 03:42:40 -0500

    Update server.R

--- server.R
+++ server.R
@@ -234,7 +234,8 @@ server <- function(input, output, session) {
       input = input,
       maker_input_id = "purchase_filter-maker",
       status_input_id = "purchase_filter-status",
-      item_name_input_id = "purchase_filter-name"
+      item_name_input_id = "purchase_filter-name",
+      sku_input_id = "purchase_filter-sku"
     )
     
     # 统计 SKU, Status, 和 PurchaseTime 下的数量
@@ -266,6 +267,7 @@ server <- function(input, output, session) {
       maker_input_id = "inbound_filter-maker",
       status_input_id = "inbound_filter-status",
       item_name_input_id = "inbound_filter-name",
+      sku_input_id = "inbound_filter-sku",
       purchase_date_range_id = "inbound_filter-purchase_date_range"
     )
     
@@ -298,6 +300,7 @@ server <- function(input, output, session) {
       maker_input_id = "outbound_filter-maker",
       status_input_id = "outbound_filter-status",
       item_name_input_id = "outbound_filter-name",
+      sku_input_id = "outbound_filter-sku",
       purchase_date_range_id = "outbound_filter-purchase_date_range"
     )
     
@@ -330,6 +333,7 @@ server <- function(input, output, session) {
       maker_input_id = "sold_filter-maker",
       status_input_id = "sold_filter-status",
       item_name_input_id = "sold_filter-name",
+      sku_input_id = "sold_filter-sku",
       purchase_date_range_id = "sold_filter-purchase_date_range"
     )
     
@@ -434,6 +438,7 @@ server <- function(input, output, session) {
       maker_input_id = "manage_filter-maker",
       status_input_id = "manage_filter-status",
       item_name_input_id = "manage_filter-name",
+      sku_input_id = "manage_filter-sku",
       purchase_date_range_id = "manage_filter-purchase_date_range"
     )
     
@@ -450,6 +455,7 @@ server <- function(input, output, session) {
       input = input,
       maker_input_id = "defect_filter-maker",
       item_name_input_id = "defect_filter-name",
+      sku_input_id = "defect_filter-sku",
       purchase_date_range_id = "defect_filter-purchase_date_range"
     )
    
@@ -483,6 +489,7 @@ server <- function(input, output, session) {
       maker_input_id = "logistic_filter-maker",
       status_input_id = "logistic_filter-status",
       item_name_input_id = "logistic_filter-name",
+      sku_input_id = "logistic_filter-sku",
       sold_date_range_id = "logistic_filter-sold_date_range",
       only_show_sold_id = "logistic_filter-only_show_sold",
       exit_date_range_id = "logistic_filter-exit_date_range",
